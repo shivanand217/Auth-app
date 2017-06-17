5a2382aff4cfbbcceadbe17078e179d8ac611551
Shiv Prakash Anand <shivakp2111@gmail.com>
2017-06-17 09:24:37 +0530

    Update database.js

--- config/database.js
+++ config/database.js
@@ -1,5 +1,4 @@
 module.exports = {
-  //database: 'mongodb://brad:brad@ds121190.mlab.com:21190/meanauthapp',   //prod
   database: 'mongodb://localhost:27017/meanauth',    //dev
   secret: 'yoursecret'
 }
